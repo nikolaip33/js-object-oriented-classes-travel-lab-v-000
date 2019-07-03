49b0e13c259c2e2013235cf5efc3d6cb5a2385ca
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-03 11:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ class Route {
   }
 
   estimatedTime(peak) {
-    peak ? return this.blocksTraveled() / 3 : console.log('off peak');
+    peak ? console.log(this.blocksTraveled): console.log('off peak');
   }
 }
